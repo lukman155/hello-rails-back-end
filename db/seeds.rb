d691c58bcb6f1d3d56dbf92a9b0a9628de6ffafb
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-06-20 08:50:38 +0100

    Add greetings controller and API endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(text: "Hello!")
+Message.create(text: "Hi there!")
+Message.create(text: "Greetings!")
+Message.create(text: "Welcome!")
+Message.create(text: "Salutations!")
